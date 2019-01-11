05ce0683e25e76103f1cd4785f050a119e8b8e00
shobeit <justinkeola84@gmail.com>
2019-01-10 17:56:41 -0800

    initial draft

---
+++ script.js
@@ -0,0 +1,235 @@
+var rand = 0;
+var word = "";
+var numWrong = 0;
+var numRight = 0;
+var phraseLength = 0;
+var numChar = 0;
+var phrases = ["Early bird gets the worm", "Read between the lines", "I can eat a horse", "Twenty-four seven", "Cat got your tounge", "One in one million", "I beg to differ", "Easier said than done", "Add insult to injury", "Don't cry over spilled milk", "Curiosity killed the cat", "Two peas in a pod", "That's the last straw", "Piece of cake", "Speak of the devil", "Go the whole nine yards", "An eye for an eye", "Hit the hay", "Stab someone in the back", "Quit cold turkey", "Cut to the chase", "Best of both worlds", "Kill two birds with one stone", "Break a leg", "Hit the nail on the head", "Kick the bucket"];     
+var movies = ["Frankly, my dear, I don't give a damn", "May the Force be with you", "Titanic", "After all this time? Always.", "Life is like a box of chocolates", "To infinity and beyond", "I'll be back", "Wax on, wax off", "I'm going to make him an offer he can't refuse", "Want to know how I got these scars?", "Citizen Kane", "Team Edward", "No, I am your father", "Do or do not. There is no try.", "Do you want to build a snowman?", "Hakuna Matata", "Go ahead, make my day", "Bond. James Bond.", "You're going to need a bigger boat", "You talkin' to me?", "Here's Johnny!", "The hills are alive!", "Toto, I have a feeling we're not in Kansas anymore", "Welcome to Jurassic Park", "Say hello to my little friend!", "Run Forest Run!"];
+var songs = ["I came in like a wrecking ball", "Yesterday all my troubles seemed so far away", "Work, work, work, work, work", "Just a small town girl", "I still haven't found what I'm looking for", "Annie are you OK?", "Nah nah nah nah nah nah nah, nah nah nah nah, hey Jude", "Is this the real life? Is this just fantasy?", "We don't need no education", "Have you ever seen the rain?", "She's got eyes of the bluest skys", "So close, no matter how far", "I don't want to miss a thing", "Take my hand, we'll make it, I swear", "That's me in the corner", "A mulatto, an albino, a mosquito, my libido", "You've been thunderstruck", "Another one bites the dust", "Eye of the tiger", "B-B-B-Bennie and the Jets", "Sing us a song you're the piano man", "Hello darkness my old friend", "Somewhere over the rainbow blue birds fly", "Don't worry about a thing", "Imagine there's no heaven", "Can you feel the love tonight?"];
+var challenges = ["A"];
+
+function sp(){
+    document.getElementById('introPage').style.display = "none";
+    document.getElementById('multiPage').style.display = "none";
+    document.getElementById('singlePage').style.display = "block";
+}
+
+function mp(){
+    document.getElementById('introPage').style.display = "none";
+    document.getElementById('singlePage').style.display = "none";
+    document.getElementById('multiPage').style.display = "block";
+}
+
+function phrase(){
+    rand = Math.floor(Math.random()*phrases.length);
+    word = phrases[rand];
+    document.getElementById('singlePage').style.display = "none";
+    document.getElementById('categoryName').innerHTML = "Phrases";
+    hangman();
+}
+
+function movie(){
+    rand = Math.floor(Math.random()*movies.length);
+    word = movies[rand];
+    document.getElementById('singlePage').style.display = "none";
+    document.getElementById('categoryName').innerHTML = "Movies and movie quotes";
+    hangman();
+}
+
+function song(){
+    rand = Math.floor(Math.random()*songs.length);
+    word = songs[rand];
+    document.getElementById('singlePage').style.display = "none";
+    document.getElementById('categoryName').innerHTML = "Song titles and lyrics";
+    hangman();
+}
+
+function countChars(countfrom,displayto) {
+    var len = document.getElementById(countfrom).value.length;
+    document.getElementById(displayto).innerHTML = len;
+}
+
+function readText(){
+    word = document.getElementById('input').value;
+    hangman();
+}
+
+function hangman(){
+    var x = word.length;
+        if(x==0){
+            alert("Please enter something into the text box.");
+            return;
+        }
+    var y = x-1;
+    var spaces = 0;
+    var validChar = new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", " ", "?", "!", ",", ".", "-", "'");
+    for(z = 0; z < word.length; z++){
+        var letter = word.substring(y,x);
+        if(validChar.indexOf(letter) > -1){
+            x--;
+            y--;
+        }
+        else{
+            alert("Please remove any special characters.");
+            return;
+        }
+    }
+    x = word.length;
+    y = x-1;
+    while (x>0){
+        numChar++;
+        var letter = word.substring(y,x);
+        if(letter === " "){
+            document.getElementById('letter'+x).innerHTML = "&nbsp;";
+            document.getElementById('letter'+x).style.visibility = "hidden";
+            document.getElementById('letter'+x).style.display = "block";
+            document.getElementById('underline'+x).style.display = "block";
+            spaces++;
+        }
+        else if(letter === "?" || letter === "!" || letter === "," || letter === "." || letter === "-" || letter === "'"){
+            document.getElementById('letter'+x).innerHTML = letter;
+            document.getElementById('letter'+x).style.display = "block";
+            document.getElementById('underline'+x).style.display = "block";
+            spaces++;
+        }
+        else{
+            document.getElementById('letter'+x).innerHTML = letter;
+            document.getElementById('letter'+x).style.visibility = "hidden";
+            document.getElementById('underline'+x).style.display = "block";            
+            document.getElementById('underline'+x).style.borderBottom = "3px solid black";
+        }
+        x--;
+        y--;
+    }
+    phraseLength = word.length - spaces;
+    document.getElementById('multiPage').style.display = "none";
+    document.getElementById('gamePage').style.display = "block";
+    splitWords();
+    document.getElementById('challengeBank').style.display = "none";
+    draw();
+}
+
+
+
+function splitWords(){
+    var placeKeep = 0;
+    var countBack = 16;
+    if(numChar > 15){
+        while(countBack > 1){
+            if(document.getElementById('letter16').innerHTML == "&nbsp;"){
+                document.getElementById('underline16').style.width = "0px";
+                document.getElementById('underline16').style.marginRight = "0px";
+            }
+            if(document.getElementById('letter'+countBack).innerHTML == "&nbsp;"){
+                document.getElementById('underline'+countBack).style.width = (document.getElementById('underline1').offsetWidth)*(16-countBack)+"px";
+                placeKeep = countBack;
+                countBack = 0;
+            }
+            countBack--;
+        }
+    }
+    for(x=0;x<8;x++){
+        countBack = 15+placeKeep;
+        if(numChar > countBack){
+            while(countBack > 1){
+                if(document.getElementById('letter'+countBack).innerHTML == "&nbsp;"){
+                    document.getElementById('underline'+countBack).style.width = (document.getElementById('underline1').offsetWidth*((16+placeKeep)-countBack))+"px";
+                    placeKeep = countBack;
+                    countBack = 0;
+                }
+                countBack--;
+            }
+        }
+    }
+    
+}
+
+function guessLetter(){
+    var correct = 0;
+    var target = event.target || event.srcElement;
+    target.style.visibility = "hidden";
+    var lower = target.id;
+    var upper = document.getElementById(lower).getAttribute('value');
+    var results = document.getElementById('results');
+    var ul1 = document.getElementById('underline1').offsetWidth;
+    for(a = 1; a < 101; a++){
+        if(document.getElementById('letter'+a).innerHTML === upper || document.getElementById('letter'+a).innerHTML === lower){
+            document.getElementById('letter'+a).style.visibility = "visible";
+            correct++;
+            numRight++;
+        }
+    }
+    if(correct==0){
+        numWrong++;
+        hang();
+    }
+
+function reset(){
+    var ul1 = document.getElementById('underline1').offsetWidth;
+    var results = document.getElementById('results');
+    var again = document.getElementById('again');
+    for(a = 1; a < 101; a++){
+        document.getElementById('letter'+a).innerHTML = "&nbsp;";
+        document.getElementById('underline'+a).style.width = ul1 + "px";
+        if(ul1 == 50){
+            document.getElementById('underline'+a).style.marginRight = "5px";
+            results.style.height = "70px";
+        }
+        else if(ul1 == 28){
+            document.getElementById('underline'+a).style.marginRight = "3px";
+            results.style.height = "50px";
+        }
+        else{
+            document.getElementById('underline'+a).style.marginRight = "3px";
+            results.style.height = "40px";
+        }
+        document.getElementById('underline'+a).style.display = "none";
+        document.getElementById('underline'+a).style.borderBottom = "0px";
+    }
+    var bank = document.getElementById("letterBank").querySelectorAll("div");
+    var cBank = document.getElementById("challengeBank").querySelectorAll("div");
+    for(b = 0; b < 26; b++){
+        bank[b].style.visibility = "visible";
+        cBank[b].style.visibility = "visible";
+    }
+    numWrong = 0;
+    numRight = 0;
+    phraseLength = 0;
+    numChar = 0;
+    results.style.marginTop = "5px";
+    results.style.lineHeight = "40px";
+    results.innerHTML = " ";
+    document.getElementById('vidSent').style.display = "none";
+    document.getElementById('letterBank').style.display = "block";
+    again.style.marginTop = "0px";
+    again.style.display = "none";
+    document.getElementById('home').style.display = "none";
+    if(phrases.indexOf(word) > -1){
+        phrases.splice(rand,1);
+        phrase();
+    }
+    else if(movies.indexOf(word) > -1){
+        movies.splice(rand,1);
+        movie();
+    }
+    else if(songs.indexOf(word) > -1){
+        songs.splice(rand,1);
+        song();
+    }
+    else if(document.getElementById('charcount').innerHTML > 0){
+        document.getElementById('gamePage').style.display = "none";
+        document.getElementById('input').value = "";
+        document.getElementById('charcount').innerHTML = "0";
+        document.getElementById('multiPage').style.display = "block";
+    }
+    else{
+        challenge();
+    }
+}
+
+function video(){
+    document.getElementById('gamePage').style.display = "none";
+    document.getElementById('videoPage').style.display = "block";
+    document.getElementById('home1').style.display = "block";
+}
